92e3a816e7b5593a2d5e455b2017b4a0d9be94a8
devransom-github <sundaystanley56@gmail.com>
2022-11-28 17:49:28 +0100

    add updates

---
+++ HtmlQuiz/quiz.js
@@ -0,0 +1,24 @@
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answersButtonElement = document.getElementById('answer-butttons')
+
+let shuffledQuestion , currentQuestionIndex;
+let QuizScore = 0;
+
+
+
+const question =[
+ {
+  question: 'which one of this is JavaScript framework?',
+  answers :[
+   {text : 'python', correct: false}
+  ]
+ }
+]
+
+
